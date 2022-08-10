91a702c65dd512b8739a4f1b36fb04e655e2286c
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-08-10 14:54:21 +0700

    Add files via upload

---
+++ week2/chapter2_2.py
@@ -0,0 +1,25 @@
+import math
+
+class Spherical:
+    def __init__(self,r):
+        self.radius = r
+
+    def changeR(self,Radius):
+        self.radius = Radius
+
+    def findVolume(self):
+        return 4/3*math.pi*self.radius**3
+
+    def findArea(self):
+        return 4*math.pi*self.radius*self.radius
+
+    def __str__(self):
+        return "Radius ="+str(self.radius)+" Volumn = "+str(self.findVolume())+" Area = "+str(self.findArea())
+
+r1, r2 = input("Enter R : ").split()
+R1 = Spherical(int(r1))
+print(type(R1))
+print(dir(R1))
+print(R1)
+R1.changeR(int(r2))
+print(R1)
