5fe5b41bdfab7cc2648ddd9bd140a4de1ff29185
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-08-12 19:32:14 +0700

    Add files via upload

---
+++ week1/minesweeper.py
@@ -0,0 +1,72 @@
+def num_grid(lst):
+    for i in range(5):
+        for j in range(5):
+            lst[i][j]=-1 if lst[i][j]=='#' else 0
+    for i in range(5):
+        for j in range(5):
+            if lst[i][j]==-1:
+                if i==0:
+                    if j==0:
+                        lst[i+1][j] = lst[i+1][j]+1 if lst[i+1][j]!=-1 else -1
+                        lst[i][j+1] = lst[i][j+1]+1 if lst[i][j+1]!=-1 else -1
+                        lst[i+1][j+1] = lst[i+1][j+1]+1 if lst[i+1][j+1]!=-1 else -1
+                    elif j==4:
+                        lst[i+1][j] = lst[i+1][j]+1 if lst[i+1][j]!=-1 else -1
+                        lst[i][j-1] = lst[i][j-1]+1 if lst[i][j-1]!=-1 else -1
+                        lst[i+1][j-1] = lst[i+1][j-1]+1 if lst[i+1][j-1]!=-1 else -1
+                    else:
+                        lst[i+1][j] = lst[i+1][j]+1 if lst[i+1][j]!=-1 else -1
+                        lst[i][j+1] = lst[i][j+1]+1 if lst[i][j+1]!=-1 else -1
+                        lst[i+1][j+1] = lst[i+1][j+1]+1 if lst[i+1][j+1]!=-1 else -1
+                        lst[i][j-1] = lst[i][j-1]+1 if lst[i][j-1]!=-1 else -1
+                        lst[i+1][j-1] = lst[i+1][j-1]+1 if lst[i+1][j-1]!=-1 else -1
+                elif i==4:
+                    if j==0:
+                        lst[i-1][j] = lst[i-1][j]+1 if lst[i-1][j]!=-1 else -1
+                        lst[i][j+1] = lst[i][j+1]+1 if lst[i][j+1]!=-1 else -1
+                        lst[i-1][j+1] = lst[i-1][j+1]+1 if lst[i-1][j+1]!=-1 else -1
+                    elif j==4:
+                        lst[i-1][j] = lst[i-1][j]+1 if lst[i-1][j]!=-1 else -1
+                        lst[i][j-1] = lst[i][j-1]+1 if lst[i][j-1]!=-1 else -1
+                        lst[i-1][j-1] = lst[i-1][j-1]+1 if lst[i-1][j-1]!=-1 else -1
+                    else:
+                        lst[i-1][j] = lst[i-1][j]+1 if lst[i-1][j]!=-1 else -1
+                        lst[i][j+1] = lst[i][j+1]+1 if lst[i][j+1]!=-1 else -1
+                        lst[i-1][j+1] = lst[i-1][j+1]+1 if lst[i-1][j+1]!=-1 else -1
+                        lst[i][j-1] = lst[i][j-1]+1 if lst[i][j-1]!=-1 else -1
+                        lst[i-1][j-1] = lst[i-1][j-1]+1 if lst[i-1][j-1]!=-1 else -1
+                else:
+                    if j==0:
+                        lst[i-1][j] = lst[i-1][j]+1 if lst[i-1][j]!=-1 else -1
+                        lst[i][j+1] = lst[i][j+1]+1 if lst[i][j+1]!=-1 else -1
+                        lst[i-1][j+1] = lst[i-1][j+1]+1 if lst[i-1][j+1]!=-1 else -1
+                        lst[i+1][j] = lst[i+1][j]+1 if lst[i+1][j]!=-1 else -1
+                        lst[i+1][j+1] = lst[i+1][j+1]+1 if lst[i+1][j+1]!=-1 else -1
+                    elif j==4:
+                        lst[i+1][j] = lst[i+1][j]+1 if lst[i+1][j]!=-1 else -1
+                        lst[i][j-1] = lst[i][j-1]+1 if lst[i][j-1]!=-1 else -1
+                        lst[i+1][j-1] = lst[i+1][j-1]+1 if lst[i+1][j-1]!=-1 else -1
+                        lst[i-1][j] = lst[i-1][j]+1 if lst[i-1][j]!=-1 else -1
+                        lst[i-1][j-1] = lst[i-1][j-1]+1 if lst[i-1][j-1]!=-1 else -1
+                    else:
+                        lst[i-1][j-1] = lst[i-1][j-1]+1 if lst[i-1][j-1]!=-1 else -1
+                        lst[i-1][j] = lst[i-1][j]+1 if lst[i-1][j]!=-1 else -1
+                        lst[i-1][j+1] = lst[i-1][j-1] if lst[i-1][j-1]!=-1 else -1
+                        lst[i+1][j] = lst[i+1][j]+1 if lst[i+1][j]!=-1 else -1
+                        lst[i][j+1] = lst[i][j+1]+1 if lst[i][j+1]!=-1 else -1
+                        lst[i+1][j+1] = lst[i+1][j+1]+1 if lst[i+1][j+1]!=-1 else -1
+                        lst[i][j-1] = lst[i][j-1]+1 if lst[i][j-1]!=-1 else -1
+                        lst[i+1][j-1] = lst[i+1][j-1]+1 if lst[i+1][j-1]!=-1 else -1
+    for i in range(5):
+        for j in range(5):
+            lst[i][j] = str(lst[i][j]) if lst[i][j]!=-1 else '#'
+    return lst
+
+print("*** Minesweeper ***")
+input_list = [i.split()for i in input("Enter input(5x5) : ").split(",")]
+print("\n")
+for i in input_list:
+    print(i)
+print("\n")
+for i in num_grid(input_list):
+    print(i)
