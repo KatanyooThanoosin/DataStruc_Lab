654be03ed490107a2c6413742bb2be73e71ca964
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-08-17 10:03:00 +0700

    Add files via upload

---
+++ week3/Parenthesis_Matching.py
@@ -0,0 +1,43 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    def isEmpty(self):
+        return len(self.items) == 0
+    
+    def size(self):
+        return len(self.items)
+    
+    def top(self):
+        return self.items[-1]
+    
+    def push(self,tmp):
+        self.items.append(tmp)
+    
+    def pop(self):
+        self.items.remove(self.items[-1])
+
+s=Stack()
+o=["(","{","["]
+c=[")","}","]"]
+a=input("Enter expresion : ")
+print(a,end="")
+for i in a:
+    if i in o:
+        s.push(i)
+    elif i in c:
+        if s.size()==0:
+            print(" close paren excess")
+            exit()
+        elif o.index(s.top())!=c.index(i):
+            print(" Unmatch open-close")
+            exit()
+        else:
+            s.pop()
+if s.size()!=0:
+    print(" open paren excess   "+str(s.size())+" : ",end="")
+    t=""
+    while not s.isEmpty():
+        t+=s.top()
+        s.pop()
+    print(t[::-1])
