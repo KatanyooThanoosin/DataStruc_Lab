d90b821adbbec6a00951f1af396891c505bd8a2b
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-08-17 11:12:07 +0700

    Update Queue_class.py

--- week4/Queue_class.py
+++ week4/Queue_class.py
@@ -12,7 +12,7 @@ class Queue:
         return self.queue[0] if self.size()>0 else -1
     
     def enqueue(self,tmp):
-        return self.queue.append(tmp)
+        self.queue.append(tmp)
     
     def dequeue(self):
-        self.queue.remove(self.queue[0])
+        return self.queue.pop(0)
