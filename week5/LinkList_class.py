04fa2ee580d4a74afce2bb14930a43f8bb2e0127
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-08-23 14:05:39 +0700

    Update LinkList_class.py

--- week5/LinkList_class.py
+++ week5/LinkList_class.py
@@ -1,1 +1,26 @@
-#ขี้เกียจเขียนอะ รอเรียนก่อนเดะมาเขียน
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def __str__(self):
+        l=[]
+        t=self.head
+        while t!=None:
+            l.append(t.data)
+            t=t.next
+        return l
+    def isEmpty(self):
+        return self.head == None
+    def append(self,data):
+        p = Node(data)
+        if self.isEmpty():
+            self.head = p
+        else:
+            t=self.head
+            while t.next!=None:
+                t=t.next
+            t.next = p
