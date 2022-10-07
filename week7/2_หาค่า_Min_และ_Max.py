365b2847ab2ad04e46f263f56031b16dd5ff211a
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-10-07 23:34:54 +0700

    Add files via upload

---
+++ week7/2_หาค่า_Min_และ_Max.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        a=Node(data)
+        if self.root is None:
+            self.root = a
+            return self.root
+        else:
+            t = self.root
+            while True:
+                if data <= t.data:
+                    if t.left == None:
+                        t.left = a
+                        return self.root
+                    else:t=t.left
+                else:
+                    if t.right == None:
+                        t.right = Node(data)
+                        return self.root
+                    else:t=t.right
+    
+    def printTree(self, node, level = 0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+    
+    def findMin(self):
+        t = self.root
+        while t.left!=None:
+            t = t.left
+        return t.data
+            
+    def findMax(self):
+        t = self.root
+        while t.right!=None:
+            t = t.right
+        return t.data
+
+T = BST()
+inp = [int(i) for i in input('Enter Input : ').split()]
+for i in inp:
+    root = T.insert(i)
+T.printTree(root)
+print("--------------------------------------------------")
+print("Min : "+str(T.findMin()))
+print("Max : "+str(T.findMax()))
