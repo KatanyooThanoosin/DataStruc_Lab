5ee685174afcf044b1ce09bc463c04f366df380f
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-11-01 12:26:05 +0700

    Update and rename 2_First_Greater_Value.py to 2_First_Greater_Value*.py

---
+++ week_10_Searching/2_First_Greater_Value*.py
@@ -0,0 +1,18 @@
+#==============================================================#
+#                                                              #
+#                                                              #
+# เกรดเดอร์ยังไม่เปิดให้ส่งเลยยังไม่ได้เทสว่าถูกมั้ย เทสแล้วเดะมาลบ คอมเมนต์นะ #
+#                                                              #
+#                                                              #
+#==============================================================#
+
+inp = input('Enter Input : ').split('/')
+arr, k = list(map(int, inp[0].split())), [int(i) for i in inp[1].split()]
+for i in k:
+    isF = True
+    for j in sorted(arr):
+        if i<j:
+            print(j)
+            isF = False
+            break
+    if isF:print("No First Greater Value")   
