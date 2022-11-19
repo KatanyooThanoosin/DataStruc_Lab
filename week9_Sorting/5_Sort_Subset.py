951b0f9c51b5aba88d100e83c39476def9f7e2d8
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-11-20 00:27:38 +0700

    Update 5_Sort_Subset.py

--- week9_Sorting/5_Sort_Subset.py
+++ week9_Sorting/5_Sort_Subset.py
@@ -1,16 +1,12 @@
 def combi(l,k,t=[],i=0):
-    if i==len(l):return [sort(t)] if sum(t)==k else []
+    if i==len(l):return [t]if sum(t)==k else []
     return combi(l,k,t,i+1)+combi(l,k,t+[l[i]],i+1)
 def moreThan(a,b):
     if len(a)!=len(b):return len(a)>len(b)
-    else:
-        for i in range(len(a)):
-            if a[i]!=b[i]:return a[i]>b[i]
-def sort(l):
-    a=[i for i in l]
-    return [a.pop(a.index(min(a))) for i in l]
+    for i in range(len(a)):
+        if a[i]!=b[i]:return a[i]>b[i]
 k,inp = input("Enter Input : ").split("/")
-l = [sort(a) for a in [list(k) for k in set(tuple(j) for j in combi([int(i)for i in inp.split()],int(k)))]]
+l = [[a.pop(a.index(min(a))) for q in a.copy()] for a in [list(k) for k in set(tuple(j) for j in combi([int(i)for i in inp.split()],int(k)))]]
 if l==[]:
     print("No Subset")
     exit()
