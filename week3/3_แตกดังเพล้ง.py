8e16615751aefc4e343e69eeaa61ede1afd1b829
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2023-07-17 21:46:02 +0700

    Update 3_แตกดังเพล้ง.py

--- week3/3_แตกดังเพล้ง.py
+++ week3/3_แตกดังเพล้ง.py
@@ -20,11 +20,6 @@ class Stack:
 l = [[int(j) for j in i.split()] for i in input("Enter input : ").split(",")]
 s = Stack()
 for i in l:
-    if s.isEmpty():
-        s.push(i)
-    else:
-        while i[0]>s.top()[0]:
-            print(s.pop()[1])
-            if s.isEmpty():
-                break
-        s.push(i)
+    while not s.isEmpty() and i[0]>s.top()[0]:
+        print(s.pop()[1])
+    s.push(i)
